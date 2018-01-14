42ef9979fc278c4cbaa140f1e7c66d3fa280683a
Marco Antônio <mjota51@gmail.com>
2018-01-14 11:31:40 -0200

    Preparing project to be a Angular Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from './app.component'
+import { CalendarModule } from './modules/calendar/calendar.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CalendarModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modules/calendar/calendar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ngx-calendar',
+  template: `
+    <p>
+      calendar works!
+    </p>
+  `,
+  styles: []
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
